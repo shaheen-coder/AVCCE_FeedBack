7af272031d2fb9bd7f84a2b591f4d3bc95552bdc
shaheen-coder <shaheenvsa@gmail.com>
2025-09-02 14:58:06 +0000

    [server] import fix

--- staff/admin.py
+++ staff/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from staff.models import Staff,ClassStaff,Subject,TimeScheduler
 from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
 # Register your models here.
+
+
+
+class SubjectResource(resources.ModelResource):
+    class Meta:
+        model = Subject
+        import_id_fields = ['code']
+
 
 @admin.register(Staff)
 class StaffAdmin(ImportExportModelAdmin):
     def get_queryset(self,request):
         qs = super().get_queryset(request)
-        if request.user.role == 'hoc' : return qs 
+        if request.user.role == 'hoc' : return qs
         elif request.user.role == 'hod' : return qs.filter(dept=request.user.dept)
         elif request.user.role == 'staff': return qs.filter(id=request.user.staff.id)
         return qs
@@ -15,15 +25,16 @@ class StaffAdmin(ImportExportModelAdmin):
 class ClassStaffAdmin(ImportExportModelAdmin):
     def get_queryset(self,request):
         qs = super().get_queryset(request)
-        if request.user.role == 'hoc' : return qs 
+        if request.user.role == 'hoc' : return qs
         elif request.user.role == 'hod' : return qs.filter(dept=request.user.dept)
         elif request.user.role == 'staff': return qs.filter(staff=request.user.staff)
         return qs
 @admin.register(Subject)
 class SubjectAdmin(ImportExportModelAdmin):
+    resource_class = SubjectResource
     def get_queryset(self,request):
         qs = super().get_queryset(request)
-        if request.user.role == 'hoc' : return qs 
+        if request.user.role == 'hoc' : return qs
         elif request.user.role == ('hod','staff') : return qs.filter(dept=request.user.dept)
         return qs
 
@@ -31,7 +42,6 @@ class SubjectAdmin(ImportExportModelAdmin):
 class TimeSchedulerAdmin(admin.ModelAdmin):
     def get_queryset(self,request):
         qs = super().get_queryset(request)
-        if request.user.role == 'hoc' : return qs 
+        if request.user.role == 'hoc' : return qs
         elif request.user.role in ('hod','staff') : return qs.filter(dept=request.user.dept)
-        return qs 
-    
+        return qs
